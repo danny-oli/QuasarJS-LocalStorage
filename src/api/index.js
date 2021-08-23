30e29f88ea23e56221dd7e3e2cf52e21ec600cc6
Danny-Home <danny.oli91@gmail.com>
2021-08-23 13:50:34 -0300

    .env file created, new Enviroment dependencies

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,18 @@
-import { date } from "quasar"
+import { date } from "quasar";
 
 export default {
-    getGithubUserByUsername: async (username) => {
-        const uri = "https://api.github.com/users/" + username;
-        const res = await fetch(uri);
-        const data = await res.json();
-        const timeStamp = Date.now();
-        data.date = date.formatDate(timeStamp, "YY-M-DD HH:mm:ss");
-        if(!data.login){
-            data.login = username;
-            data.name = 'Not Found';
-            data.type = data.message;
-            data.avatar_url= require('../assets/images/user-not-found.jpg')
-        }
-        return data
+  getGithubUserByUsername: async (username) => {
+    const uri = process.env.GITHUB_API_URL + username;
+    const res = await fetch(uri);
+    const data = await res.json();
+    const timeStamp = Date.now();
+    data.date = date.formatDate(timeStamp, "YY-M-DD HH:mm:ss");
+    if (!data.login) {
+      data.login = username;
+      data.name = "Not Found";
+      data.type = data.message;
+      data.avatar_url = require("../assets/images/user-not-found.jpg");
     }
-}
+    return data;
+  },
+};
